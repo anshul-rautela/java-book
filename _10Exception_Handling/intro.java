5f82438dea4747a0e297050fb89528536527ad99
Anshul Rautela <anshulrautela123456789@gmail.com>
2024-10-21 18:05:28 +0530

    exeption handle

--- _10Exception_Handling/intro.java
+++ _10Exception_Handling/intro.java
@@ -1,6 +1,12 @@
 package _10Exception_Handling;
 public class intro {
     public static void main(String []args){
-        
+        try{
+            int a = 3/0;
+        }
+        catch(ArithmeticException e){
+            System.out.println("This cant divide by zero");
+        }
+        System.out.println("after catch");
     }
 }
